71a6bd24888bb70ba98b4867501eac5ec2138468
jemiezler <142890376+jemiezler@users.noreply.github.com>
2024-08-30 05:37:37 +0000

    frontend-user: [re-created] initial project

--- frontend/user/app/_layout.tsx
+++ frontend/user/app/_layout.tsx
@@ -4,7 +4,6 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-import "../global.css"
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
