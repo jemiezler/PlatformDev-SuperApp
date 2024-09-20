acf4967c20adcce394dce51afc32b017cc195763
Nattawat Nattachanasit <6531503201@lamduan.mfu.ac.th>
2024-09-20 23:23:41 +0700

    frontend-user: [modify] login page

--- frontend/user/app/login.tsx
+++ frontend/user/app/login.tsx
@@ -1,36 +1,47 @@
-// screens/SignIn.tsx
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { router } from 'expo-router';
-import { useSession } from '@/contexts/ctx';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button, Card, H2, Image, Input, Paragraph, Text, View, XStack, YStack } from 'tamagui'
+import { router } from "expo-router";
+import { useSession } from "@/contexts/ctx";
+import { Button, Card, H3, Input, Sheet, Text, View, YStack } from "tamagui";
 
 export default function SignIn() {
   const { logIn } = useSession();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [open, setOpen] = useState(true);
+  const [modal, setModal] = useState(true);
 
   const handlelogIn = async () => {
     try {
       await logIn(email, password);
-      router.replace('/'); // Navigate to the home page or another route after successful sign-in
+      router.replace("/");
     } catch (error) {
-      // Handle sign-in error (e.g., show an alert or message)
       console.error("Sign-in error:", error);
     }
   };
 
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center', backgroundColor: 'grey', }}>
-      <View padding="$1">
-        <Card padded style={{ width: '100%', borderRadius: '24px' }}>
-          <H2 paddingBottom="20px">Get Started</H2>
-          <YStack paddingBottom="20px">
-            <Text fontWeight="700">Welcome to MFU Campus!</Text>
-            <Text >Your gateway to campus life and services.</Text>
+    <YStack
+      style={{
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+        backgroundColor: "grey",
+      }}
+    >
+      <Sheet
+        modal={modal}
+        open={open}
+        snapPoints={undefined}
+        snapPointsMode={'fit'}
+      >
+        <Card padded borderRadius="$8">
+          <H3 paddingBottom="$2">Get Started</H3>
+          <YStack paddingBottom="$4">
+            <Text>Welcome to MFU Campus!</Text>
+            <Text>Your gateway to campus life and services.</Text>
           </YStack>
-          <YStack gap="$2" paddingBottom="20px">
+          <YStack gap="$2" paddingBottom="$4">
             <Input
               placeholder="Email"
               value={email}
@@ -45,10 +56,17 @@ export default function SignIn() {
               style={{ borderBottomWidth: 1 }}
             />
           </YStack>
-          <Button borderRadius="$6" marginBottom="10px" onPress={handlelogIn} >Continue</Button>
-          <Button borderRadius="$6" >Continue</Button>
+          <Button
+            borderRadius="$6"
+            marginBottom="$2"
+            color="white"
+            onPress={handlelogIn}
+          >
+            Continue
+          </Button>
+          <Button borderRadius="$6">Continue With Google</Button>
         </Card>
-      </View>
-    </SafeAreaView>
+      </Sheet>
+    </YStack>
   );
 }
